b6991287e5b02442c87e2868791d6fb7d74c8e47
jdrichards-pursuit <jdrpursuit@gmail.com>
2024-03-07 13:21:21 -0500

    add title

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
-  return <div>Tuner Frontend</div>
-}
+  return <div>Tunr Frontend</div>;
+};
 
-export default App
+export default App;
