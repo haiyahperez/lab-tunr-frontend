350e0decd02c62636f453b0f026fb144dd83f2fd
Haiyah Perez <haiyahperez@pursuit.org>
2024-03-17 23:20:03 -0400

    set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import React from "react";
 
+import NavBar from "./Components/NavBar"
 const App = () => {
   return <div>Tunr Frontend</div>;
 };
